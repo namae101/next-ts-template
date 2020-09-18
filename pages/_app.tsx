11df09f399dd31ef4185da0619d9414f61c01c89
namae101 <sovannarongkoek@gmail.com>
2020-09-18 15:38:26 +0700

    add eslint to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/index.css"; // <- applied everywhere in the NextJS application scope
+import type { AppProps /* , AppContext */ } from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,24 @@
-import Head from 'next/head'
-import tw, {styled} from 'twin.macro';
+import tw, { styled } from "twin.macro";
 
 interface TitleProps {
-  isBold? :boolean;
+  isBold?: boolean;
   isPrimary?: boolean;
 }
 
-const Title = styled.h1<TitleProps>(({isBold, isPrimary }) => [
+const Title = styled.h1<TitleProps>(({ isBold, isPrimary }) => [
   tw`mt-5 ml-5 px-4 py-2 text-5xl`,
   // Ternary
   isBold ? tw`font-bold` : tw`font-semibold`,
   // Conditional Style
-  isPrimary && tw`text-blue-500`
+  isPrimary && tw`text-blue-500`,
 ]);
 
-const Home = () => {
+const Home: React.FunctionComponent = () => {
   return (
     <div>
       <p>hello</p>
       <Title isBold>Title</Title>
       <Title isPrimary>Title</Title>
-
     </div>
   );
 };
